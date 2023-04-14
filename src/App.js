ea4460b98b037aed14f8b9cb288da695be452ff7
chandnisingh-design <chandnisingh591@gmail.com>
2023-04-14 15:02:03 +0530

    reacttowebproject

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "./Components/WebComponent1";
+
+function App() {
+  return (
+    <div className="App">
+      <web-greeting name = "Amit Singh"></web-greeting>
+    </div>
+  );
+}
+
+export default App;
